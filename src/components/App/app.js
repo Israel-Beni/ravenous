7ade32166cac495125b8d82683f9fd0827d0be1e
Israel-Beni <bendegbe72@gmail.com>
2021-12-03 03:36:58 +0530

    Business, BusinessList, SearchBar components injected the into App component

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import logo from '../../logo.svg';
 import './app.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App(props) {
   return (
