6e0e14a91bf3ab75aa93951583ef9fa713ae1d10
Israel-Beni <bendegbe72@gmail.com>
2021-12-10 01:33:31 +0530

    Yelp API integrated and connected to the website: Searh of Business functionality completed and operational

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,32 +1,38 @@
 const apiKey = 'd63bbESUMV8GvTzYeTXvg5LoFkATfAVU18QNlZQmod7oCOMNzg9cr_Twu6jOXjYwqfvG2IhRzgpOlJ1CvPc0BrfnZ9SVxTZuBcPUGzPNikpoVlkCuvIxWesPS8KxYXYx';
+const clientId = 'lx8TYRuT1khynvqwMBaLWw';
 
 const Yelp = {
     search(term, location, sortBy) {
-        const endpoint = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&locaiton=&{location}&sort_by=${sortBy}`;
+        const endpoint = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
         const authentication = {
+            method: 'GET',
             headers: {
-                Authorization: `Bearer ${apiKey}`
+                "accept": "application/json",
+                "x-requested-with": "xmlhttprequest",
+                "Access-Control-Allow-Origin":"http://localhost:3000/",
+                "Authorization": `Bearer ${apiKey}`
             }
         };
         return fetch(endpoint, authentication)
                     .then(response => {
-                        return response.json();
-                    }).then (jsonResponse => {
+                        const jsonResponse = response.json();
+                        console.log('response: ', jsonResponse);
+                        return jsonResponse;
+                    }).then(jsonResponse => {
                         if (jsonResponse.businesses) {
-                            return jsonResponse.businesses.map( business => {
-                                return {
-                                    id: business.id,
-                                    imageSrc: business.imageUrl,
-                                    name: business.name,
-                                    address: business.location.address1,
-                                    city: business.location.city,
-                                    state: business.location.state,
-                                    zipCode: business.location.zip_code,
-                                    category: business.categories.title,
-                                    rating: business.rating,
-                                    reviewCount: business.review_count
-                                }
-                            });
+                            console.log('jsonResponse:', jsonResponse)
+                            return jsonResponse.businesses.map( business => ({
+                                id: business.id,
+                                imageSrc: business.image_url,
+                                name: business.name,
+                                address: business.location.address1,
+                                city: business.location.city,
+                                state: business.location.state,
+                                zipCode: business.location.zip_code,
+                                category: business.categories[0].title,
+                                rating: business.rating,
+                                reviewCount: business.review_count
+                            }));
                         }
                     });
     }
